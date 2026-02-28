adabddc0b9590449b6ca3697b20e5ff2c777dde6
Luca DalCanto <111649024+Luca-Skyline@users.noreply.github.com>
2026-02-28 12:25:58 -0500

    Office hours updates
    - proportional control with two sensors
    - iffy intersection detection and turning

--- robotcode.py
+++ robotcode.py
@@ -3,6 +3,7 @@ from buildhat import ColorSensor
 from basehat import LineFinder
 from basehat import IMUSensor
 from basehat import HallSensor
+from basehat import UltrasonicSensor
 
 import time
 import math
@@ -17,31 +18,85 @@ state = "STOPPED"
 
 # Ultrasnoic Sesnors
 frontUltra = UltrasonicSensor(26)
-sideUltra = UltrasonicSensor(18)
+rightUltra = UltrasonicSensor(18)
+leftUltra = UltrasonicSensor(24)
 
 motorL = Motor('A') #Left motor
 motorR = Motor('B') #Right motor
 
-TARGETDIST = 10 #in cm, dist from the wall
+TARGETDIST = 0 #in cm, dist from the wall
 
 kP = 0.6
 
-SPEED = 80
+SPEED = 20
+
+try:
+    TRACKWIDTH = rightUltra.getDist + leftUltra.getDist
+except:
+    print('Bad calibration')
+    TRACKWIDTH = 20
+
 
 def main():
-    print("egg")
+    try:
+        while True:
+            drive()
+            turnAtIntersection()
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        motorL.stop()
+        motorR.stop()
+        print("\nCtrl+C detected. Exiting...")
+
 
 # Use just the P constant in PID to correct heading
 # Option 1: Use gyroscope (area underneath the Gyro Z curve)
 # Option 2: Have it be a certain dist from wall
 def drive():
-    dist = frontUltra.getDist
+    rightDist = rightUltra.getDist
+    leftDist = leftUltra.getDist
+    
+    if rightDist is None and leftDist is not None:
+        print('Right out of range')
+        dist = leftDist
+        error = (TRACKWIDTH / 2) - dist
+    elif leftDist is None and rightDist is not None:
+        print('Left out of range')
+        dist = rightDist
+        error = dist - (TRACKWIDTH / 2)
+    elif leftDist is None and rightDist is None:
+        motorR.stop()
+        motorL.stop()
+        return
+    else:
+        error = rightDist - leftDist
 
-    error = dist - TARGETDIST
     correction = error * kP
+    
+    if(SPEED - correction < 0):
+        correction = SPEED
+    
+    if(SPEED - correction > 100):
+        correction = 100 - SPEED
 
-    motorR.start(SPEED + correction)
-    motorL.start(SPEED - correction)
+    motorR.start(SPEED - correction)
+    motorL.start(-SPEED - correction)
+    
+def turnAtIntersection():
+    dist = frontUltra.getDist
+    
+    if dist is not None:
+        if dist < TRACKWIDTH / 2:
+            if leftUltra.getDist is not None:
+                direction = 'R'
+                print('TURNING RIGHT')
+            else:
+                direction = 'L'
+                print('TURNING LEFT')
+            while(dist is not None and dist < 100):
+                turn(direction, SPEED)
+                dist = frontUltra.getDist
+            print('DONE TURNING')
 
 # Stop Driving
 def stop():
@@ -60,5 +115,7 @@ def turn(direction, speed):
     lMult = -1 * rMult
 
     # To turn about itself the robot needs to have left and right going in opposite directions
-    motorL.start(rMult * speed)
+    motorL.start(-rMult * speed)
     motorR.start(lMult * speed)
+    
+main()
